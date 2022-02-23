059ab59fdda3a4f6011835f3b3601772556a4f09
Obed Developer <75869356+xXObedXx@users.noreply.github.com>
2022-02-23 13:28:40 -0500

    Setup 3

---
+++ build/server.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.config();
+    }
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+    routers() {
+        this.app.listen(this.app.get('port'), () => console.log('Server On Port', this.app.get('port')));
+    }
+    start() {
+    }
+}
+const server = new Server();
+server.start();
