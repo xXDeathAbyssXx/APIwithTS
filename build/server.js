cfb4aaace09f98ef5260fc9e98759563d8e5e9cf
Obed Developer <75869356+xXObedXx@users.noreply.github.com>
2022-02-24 20:01:34 -0500

    Coding...

--- build/server.js
+++ build/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
@@ -13,44 +15,59 @@ const compression_1 = __importDefault(require("compression"));
 const cors_1 = __importDefault(require("cors"));
 require("dotenv").config();
 class Server {
-    constructor() {
-        this.app = (0, express_1.default)();
-        this.config();
-        this.routers();
-    }
-    config() {
-        //MongoDB
-        const user = process.env.USERDB;
-        const pass = process.env.PASSWORDDB;
-        mongoose_1.default.connect(`mongodb+srv://` + user + `:` + pass +
-            `@cluster0.pmfuk.mongodb.net/Data?retryWrites=true&w=majority`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-        }, () => {
-            console.log("\n");
-            console.log(`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`);
-            console.log("[INFO]: Loading Mondodb....");
-            console.log(`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`);
-            console.log("[INFO]: Ready MongoDB ✅");
-            console.log(`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`);
-        });
-        //Backend Settings
-        this.app.set('port', process.env.PORT || 3000);
-        this.app.use((0, morgan_1.default)('dev'));
-        this.app.use(express_1.default.json());
-        this.app.use((0, helmet_1.default)());
-        this.app.use(express_1.default.urlencoded({ extended: true }));
-        this.app.use((0, compression_1.default)());
-        this.app.use((0, cors_1.default)());
-    }
-    routers() {
-        this.app.use(indexRouters_1.default);
-        this.app.use('/api/register', UsersRoutes_1.default);
-    }
-    start() {
-        this.app.listen(this.app.get('port'), () => console.log('Server On Port: ' + this.app.get('port')));
-    }
+  constructor() {
+    this.app = (0, express_1.default)();
+    this.config();
+    this.routers();
+  }
+  config() {
+    //MongoDB
+    const user = process.env.USERDB;
+    const pass = process.env.PASSWORDDB;
+    mongoose_1.default.connect(
+      `mongodb+srv://` +
+        user +
+        `:` +
+        pass +
+        `@cluster0.pmfuk.mongodb.net/Data?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+      () => {
+        console.log("\n");
+        console.log(
+          `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+        );
+        console.log("[INFO]: Loading Mondodb....");
+        console.log(
+          `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+        );
+        console.log("[INFO]: Ready MongoDB ✅");
+        console.log(
+          `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+        );
+      }
+    );
+    //Backend Settings
+    this.app.set("port", process.env.PORT || 3000);
+    this.app.use((0, morgan_1.default)("dev"));
+    this.app.use(express_1.default.json());
+    this.app.use((0, helmet_1.default)());
+    this.app.use(express_1.default.urlencoded({ extended: true }));
+    this.app.use((0, compression_1.default)());
+    this.app.use((0, cors_1.default)());
+  }
+  routers() {
+    this.app.use(indexRouters_1.default);
+    this.app.use("/api/register", UsersRoutes_1.default);
+  }
+  start() {
+    this.app.listen(this.app.get("port"), () =>
+      console.log("Server On Port: " + this.app.get("port"))
+    );
+  }
 }
 const server = new Server();
 server.start();
--- src/routers/UsersRoutes.ts
+++ src/routers/UsersRoutes.ts
@@ -1,62 +1,51 @@
-import {Request, Response, Router} from 'express';
+import { Request, Response, Router } from 'express';
 import mongoose from 'mongoose';
 import RegisterUserDB from '../models/Registers';
 import axios from 'axios';
 
-class UsersRoutes{
+class UsersRoutes {
     router: Router;
     constructor() {
-    this.router = Router();
-    this.routes();
+        this.router = Router();
+        this.routes();
     }
 
     public async getAllRes(req: Request, res: Response) {
-    const Users = await RegisterUserDB.find();
-    res.json(Users);
+        const Users = await RegisterUserDB.find();
+        return res.json(Users);
     }
 
-    public async getRes(req: Request, res: Response){
-    const resuser = await RegisterUserDB.findOne({username: req.params.username})
-    res.json(resuser)
-    let testingname = "testingname2"
-    axios.get(`http://localhost:3000/api/register/${testingname}`).then(
-    function(response) {
-    let rbxuserdata = response.data;
-    let rbxusername = rbxuserdata.username;
-    console.log(response.data)
-    console.log('GG WORKING')
-    return
-    }).catch(function(error){
-    console.log(error)
-    });
+    public async getRes(req: Request, res: Response) {
+        const resuser = await RegisterUserDB.findOne({ username: req.params.username })
+        console.log(resuser)
+        res.json(resuser);
     }
 
     public async createRes(req: Request, res: Response) {
-    const {username, balance, offers, reffered,inviter} = req.body;
-    const newReg = new RegisterUserDB({username, balance, offers, reffered,inviter});
-    await newReg.save();
-    res.json({newReg});
-    
+        const { username, balance, offers, reffered, inviter } = req.body;
+        const newReg = new RegisterUserDB({ username, balance, offers, reffered, inviter });
+        await newReg.save();
+        return res.json({ newReg });
     }
-  
-    public async updateRes(req:Request, res: Response){
-    const {username} = req.params;
-    const updateuser = await RegisterUserDB.findOneAndUpdate({username}, req.body, {new: true})
-    res.json(updateuser)
+
+    public async updateRes(req: Request, res: Response) {
+        const { username } = req.params;
+        const updateuser = await RegisterUserDB.findOneAndUpdate({ username }, req.body, { new: true })
+        return res.json(updateuser)
     }
 
     public async deleteRes(req: Request, res: Response) {
-    const {username} = req.params;
-    const deleteuser = await RegisterUserDB.findOneAndDelete({username})
-    res.json(deleteuser)
+        const { username } = req.params;
+        const deleteuser = await RegisterUserDB.findOneAndDelete({ username })
+        return res.json(deleteuser)
     }
 
-    routes(){
-    this.router.get('/', this.getAllRes);
-    this.router.get('/:username', this.getRes);
-    this.router.post('/', this.createRes);
-    this.router.put('/:username', this.updateRes);
-    this.router.delete('/:username', this.deleteRes);
+    routes() {
+        this.router.get('/', this.getAllRes);
+        this.router.get('/:username', this.getRes);
+        this.router.post('/', this.createRes);
+        this.router.put('/:username', this.updateRes);
+        this.router.delete('/:username', this.deleteRes);
     }
 
 }
